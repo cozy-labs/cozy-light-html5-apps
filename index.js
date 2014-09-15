d85a31686ba1141bc27018f11c309b0ee2518c3c
Frank Rousseau <frank.rousseau@cozycloud.cc>
2014-09-15 23:48:21 +0200

    make the plugin works

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 express = require('express');
+path = require('path');
 
 module.exports.configureAppServer = function(app, config, routes, callback) {
+
   Object.keys(config.apps).forEach(function (appId) {
-    var app = config.apps[appId];
-    if(app.type === "static") {
-      console.log(app);
-      console.log(config);
-      //app.use("/public", express.static(__dirname + "/public"));
+    staticApp = config.apps[appId];
+    if(staticApp.type === "static") {
+      var configDir = path.dirname(module.exports.configPath);
+      var appPath = path.join(configDir, 'node_modules');
+      appPath = path.join(appPath, staticApp.name);
+      console.log(staticApp.name);
+      console.log(appPath);
+      app.use("/apps/" + staticApp.name, express.static(appPath));
     }
   });
   callback();
 };
 
 
-module.exports.getTemplate = function() {};
-module.exports.configure = function(options, config, program) {};
+module.exports.getTemplate = function() {
+  return '';
+};
+
+module.exports.configure = function(options, config, program) {
+  module.exports.config = config;
+  module.exports.configPath = options.config_path;
+}
