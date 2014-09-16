c0c73c6926b6a671aa67fb1ae90cfb41ba5768ff
Frank Rousseau <frank.rousseau@cozycloud.cc>
2014-09-16 23:29:38 +0200

    remove useless stuff

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ express = require('express');
 path = require('path');
 
 module.exports.configureAppServer = function(app, config, routes, callback) {
-
   Object.keys(config.apps).forEach(function (appId) {
     staticApp = config.apps[appId];
     if(staticApp.type === "static") {
       var configDir = path.dirname(module.exports.configPath);
       var appPath = path.join(configDir, 'node_modules');
       appPath = path.join(appPath, staticApp.name);
-      console.log(staticApp.name);
-      console.log(appPath);
       app.use("/apps/" + staticApp.name, express.static(appPath));
     }
   });
